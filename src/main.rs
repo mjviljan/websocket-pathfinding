6694cc2ac194ca479e7b000f3eeb5c780ec59925
Mika Viljanen <mika.viljanen@wunderdog.fi>
2022-01-22 21:00:34 +0200

    Add check for required POST request parameter

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,43 @@
 use std::collections::HashMap;
 
-use warp::Filter;
+use warp::{Filter, reject, Rejection, reply, Reply};
+use warp::http::StatusCode;
 
 const CLIENT_FOLDER: &str = "client/";
 
-async fn register_new_user(form_data: HashMap<String, String>) -> Result<impl warp::Reply, warp::Rejection> {
+#[derive(Debug)]
+enum ApiError {
+    UsernameMissing
+}
+
+impl reject::Reject for ApiError {}
+
+async fn register_new_user(form_data: HashMap<String, String>) -> Result<impl Reply, Rejection> {
     println!("Got registration request...");
-    println!("Got body: {:?}", form_data);
-    Ok(warp::reply::with_status(format!("Registration done for {}", form_data.get("username").unwrap()), warp::http::StatusCode::OK))
+    match form_data.get("username") {
+        Some(username) => Ok(reply::with_status(format!("Registration done for {}", username), StatusCode::OK)),
+        None => Err(reject::custom(ApiError::UsernameMissing))
+    }
+}
+
+async fn handle_rejection(err: Rejection) -> Result<impl Reply, Rejection> {
+    println!("Handling error: {:?}", err);
+
+    if let Some(ApiError::UsernameMissing) = err.find() {
+        Ok(reply::with_status("ERROR: No username was given", StatusCode::BAD_REQUEST))
+    } else {
+        Err(err)
+    }
 }
 
 #[tokio::main]
 async fn main() {
     let register = warp::post()
         .and(warp::path("register"))
-        .and(warp::body::form::<HashMap<String, String>>())
-        .and_then(register_new_user);
+        .and(warp::body::content_length_limit(128))
+        .and(warp::body::form())
+        .and_then(register_new_user)
+        .recover(handle_rejection);
 
     let api = register;
 
